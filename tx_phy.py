5d1a84637a1aa7fb734c25a3b53edac794707561
aganji <anirudh.ganji@gmail.com>
2018-02-22 13:13:00 -0500

    Class form of phy tx

--- tx_phy.py
+++ tx_phy.py
@@ -1,48 +1,55 @@
 import RPi.GPIO as GPIO
 import time
 
+class vlc_tx(object):
+	"""docstring for vlc_tx"""
+	def __init__(self, arg):
+		super(vlc_tx, self).__init__()
+		#self.arg = arg
 
-txDelay = 1
-ledPin = 3
-fecFactor = 3
-enableManchester = True
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(ledPin,GPIO.OUT)
+		self.txDelay = 1
+		self.ledPin = 3
+		self.fecFactor = 3
+		self.enableManchester = True
+
+		GPIO.setmode(GPIO.BOARD)
+		GPIO.setup(ledPin,GPIO.OUT)
 
 
-def fec_bit(bit):
-	
-	opList = []
-	bitVal = int(bit)
-	for x in range(fecFactor):
-		opList.append(bitVal)
-	
-	return opList
+	def fec_bit(bit):
+		
+		opList = []
+		bitVal = int(bit)
+		for x in range(self.fecFactor):
+			opList.append(bitVal)
+		
+		return opList
 
-def send_data(bit_stream,enableManchester=True):
-	for bit in bit_stream:
-		op = fec_bit(bit)
-		for bitVal in op:
-			send_bit(bitVal,enableManchester)
+	def send_data(bit_stream,enableManchester=self.enableManchester):
+		for bit in bit_stream:
+			op = fec_bit(bit)
+			for bitVal in op:
+				send_bit(bitVal,enableManchester)
 
-def send_bit(bit,enableManchester=True):
-	if enableManchester == True:
-		if bit == 1:
-			GPIO.output(ledPin,GPIO.HIGH)
-			time.sleep(txDelay)
-			GPIO.output(ledPin,GPIO.LOW)
-			time.sleep(txDelay)	
-		elif bit == 0:
-			GPIO.output(ledPin,GPIO.LOW)
-			time.sleep(txDelay)
-			GPIO.output(ledPin,GPIO.HIGH)
-			time.sleep(txDelay)
-	else:
-		if bit == 1:
-			GPIO.output(ledPin,GPIO.HIGH)
-			time.sleep(txDelay)
-		elif bit == 0:
-			GPIO.output(ledPin,GPIO.LOW)
-			time.sleep(txDelay)
+	def send_bit(bit,enableManchester=self.enableManchester):
+		if enableManchester == True:
+			if bit == 1:
+				GPIO.output(self.ledPin,GPIO.HIGH)
+				time.sleep(self.txDelay)
+				GPIO.output(self.ledPin,GPIO.LOW)
+				time.sleep(self.txDelay)	
+			elif bit == 0:
+				GPIO.output(self.ledPin,GPIO.LOW)
+				time.sleep(self.txDelay)
+				GPIO.output(self.ledPin,GPIO.HIGH)
+				time.sleep(self.txDelay)
+		else:
+			if bit == 1:
+				GPIO.output(self.ledPin,GPIO.HIGH)
+				time.sleep(self.txDelay)
+			elif bit == 0:
+				GPIO.output(self.ledPin,GPIO.LOW)
+				time.sleep(self.txDelay)
 
-GPIO.cleanup()
+	def cleanuo():
+		GPIO.cleanup()
